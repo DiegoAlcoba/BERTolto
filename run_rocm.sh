04615161753021cacb5b337ecf71a66ccaaf9b2c
DiegoAlcoba <diegoalcobaarias@gmail.com>
2025-10-17 08:40:17 +0200

    Celdas de entrenamiento, calibración y validación preparadas

--- run_rocm.sh
+++ run_rocm.sh
@@ -9,9 +9,11 @@ docker run -it --rm \
   --user $(id -u):$(id -g) \
   -v $HOME:/workspace \
   -v $HOME/.cache/pip:/root/.cache/pip \
-  -v $HOME/.cache/huggingface:/root/.cache/huggingface \
   -v $HOME/BERTolto/checkpoints:/checkpoints \
+  -w /workspace/BERTolto \
+  -e HF_HOME=/workspace/BERTolto/.cache/huggingface \
+  -e HUGGINGFACE_HUB_CACHE=/workspace/BERTolto/.cache/huggingface \
+  -e TRANSFORMERS_CACHE=/workspace/BERTolto/.cache/huggingface/hub \
   -e TOKENIZERS_PARALLELISM=false \
   -e HF_HUB_DISABLE_TELEMETRY=1 \
-  -w /workspace/BERTolto \
   bertolto:rocm7 bash
